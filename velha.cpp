028a7ca8157bbbb9cd4c936005f2233c0d81e5fb
Arthur <arthur.henrique529@gmail.com>
2025-06-04 22:34:18 -0300

    Sucesso na primeira vitória de O

--- velha.cpp
+++ velha.cpp
@@ -4,6 +4,7 @@ int CheckTicTacToeResult(int hash[3][3]) {
     for (int i = 0; i < 3; ++i) {
     if (hash[i][0] == hash[i][1] && hash[i][1] == hash[i][2]) {
         if (hash[i][0] == 1) return 1; // X venceu
+        if (hash[i][0] == 2) return 2; // O venceu
     }
 }
 
