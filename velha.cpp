8f3a30800f7d053db38f575874364677c914ba29
Arthur <arthur.henrique529@gmail.com>
2025-06-06 10:09:27 -0300

    Teste de vitória de O por todas as diagonais

--- velha.cpp
+++ velha.cpp
@@ -17,10 +17,12 @@ int CheckTicTacToeResult(int hash[3][3]) {
 
     if (hash[0][0] == hash[1][1] && hash[1][1] == hash[2][2] && hash[0][0] != 0) {
         if (hash[0][0] == 1) return 1;
+        if (hash[0][0] == 2) return 2;
 
 }
     if (hash[0][2] == hash[1][1] && hash[1][1] == hash[2][0] && hash[0][2] != 0) {
         if (hash[0][2] == 1) return 1;
+        if (hash[0][0] == 2) return 2;
    
 }
 
